ec17d8afa76019080d0b87ca39da0d349c30461f
Kingxtrem <tantubaykingshuk@gmail.com>
2025-03-10 14:03:25 +0530

    new repl added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='container d-flex justify-content-center bg-primary'>
+    <div>lets check this</div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App

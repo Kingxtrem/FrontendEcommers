9368830f84f23e084da7e580fe60e72eb0b98d31
Kingxtrem <tantubaykingshuk@gmail.com>
2025-03-24 18:52:05 +0530

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<Products />} />
+        <Route path='*' element={<Home/>} />
 
 
       </Routes>

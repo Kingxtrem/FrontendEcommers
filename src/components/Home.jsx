eac5118ae9a0c48be75d115068fc4ab069084b4c
Kingxtrem <tantubaykingshuk@gmail.com>
2025-03-24 22:58:13 +0530

    testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 const Home = () => {
   const navigate = useNavigate()
   const productPage = () => {
-    navigate('/Products')
+    // navigate('/Products')
   }
 
   return (

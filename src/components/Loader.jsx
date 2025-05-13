ed50ffd5662f3ea5bc3a263b992ded70f5dab42e
Kingxtrem <tantubaykingshuk@gmail.com>
2025-05-14 04:02:13 +0530

    loaded updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { AiOutlineLoading } from "react-icons/ai";
 
 const Loader = () => {
     return (
-        <div className="flex justify-center items-center mx-auto">
+        <div className="flex justify-center items-center mx-auto w-full h-screen">
             <div className="flex items-center rounded-xl p-5 text-red-600 text-9xl font-extrabold">
                 <AiOutlineLoading className="animate-spin" />
             </div>

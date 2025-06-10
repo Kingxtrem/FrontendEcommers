c902200240f8d32c0d2792842093ed5bf2dfdaed
Kingxtrem <tantubaykingshuk@gmail.com>
2025-06-10 14:45:55 +0530

    backend baseurl changed

--- src/axios/Api.jsx
+++ src/axios/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = "https://backend-63h6.onrender.com"
-// const baseUrl = "http://localhost:5000"
+// const baseUrl = "https://backend-63h6.onrender.com"
+const baseUrl = "http://localhost:5000"
 const Api = axios.create({
     baseURL: baseUrl
 });

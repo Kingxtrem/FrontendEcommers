606905f48e1ba8e1ce8db12844d58ecad1f832cf
Kingxtrem <tantubaykingshuk@gmail.com>
2025-06-16 03:54:03 +0530

    hero h changed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 
 const HeroSection = () => (
-    <section className="bg-gradient-to-br from-blue-100 to-green-100 py-16 px-4 flex flex-col items-center justify-center text-center rounded-xl h-[75vh]">
-        <h1 className="text-4xl md:text-6xl font-extrabold mb-4 font-sans" style={{ fontFamily: "'Roboto', sans-serif" }}>
+    <section className="bg-gradient-to-br from-blue-100 to-green-100 py-16 px-4 flex flex-col items-center justify-center text-center rounded-xl h-[60vh]">
+        <h1 className="text-4xl md:text-6xl font-extrabold mb-4 ">
             Discover the Latest Tech at <span className="text-blue-700">TechCart</span>
         </h1>
         <p className="text-lg md:text-2xl text-gray-700 mb-8 max-w-2xl">
@@ -10,7 +10,7 @@ const HeroSection = () => (
         </p>
         <div className="flex gap-4">
             <Link to="/products">
-                <button className='animate-bounce hover:animate-none rounded-xl text-3xl p-3 bg-blue-700 text-white cursor-pointer hover:bg-blue-800 active:bg-blue-950 transition duration-300 text-nowrap'>Shop Now</button>
+                <button className='animate-pulse hover:animate-none rounded-xl text-3xl p-3 bg-blue-700 text-white cursor-pointer hover:bg-blue-800 active:bg-blue-950 transition duration-300 text-nowrap'>Shop Now</button>
             </Link>
         </div>
     </section>
